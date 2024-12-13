9a131b8c484606dd3f946a6c9bb404156a4d5e5b
Ritam Ghosh <ritam.ghosh2709@gmail.com>
2024-12-13 21:30:14 +0530

    new\

--- src/main/java/com/ritam/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/ritam/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -9,7 +9,8 @@ public class FunRestController {
     @GetMapping("/")
     public String sayHello(){
 
-        return "Hello World";
+        return "Hello World this is Spring";
+
     }
 //dhur bal
 //gandur
